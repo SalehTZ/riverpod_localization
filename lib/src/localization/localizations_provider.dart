51b1916284d154589d136db7c51588b9f85993b2
SalehTZ <m.s_ta@live.com>
2024-05-25 11:37:46 +0330

    Created localization provider.

--- lib/src/localization/localizations_provider.dart
+++ lib/src/localization/localizations_provider.dart
@@ -2,10 +2,13 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'localizations_provider.g.dart';
 
 /// provider used to access the AppLocalizations object for the current locale
-final appLocalizationsProvider = Provider<AppLocalizations>((ref) {
+@Riverpod(keepAlive: true)
+AppLocalizations appLocalization(AppLocalizationRef ref) {
   // 1. initialize from the initial locale
   ref.state = lookupAppLocalizations(ui.PlatformDispatcher.instance.locale);
   // 2. create an observer to update the state
@@ -18,7 +21,7 @@ final appLocalizationsProvider = Provider<AppLocalizations>((ref) {
   ref.onDispose(() => binding.removeObserver(observer));
   // 4. return the state
   return ref.state;
-});
+}
 
 /// observer used to notify the caller when the locale changes
 class _LocaleObserver extends WidgetsBindingObserver {
---
+++ lib/src/localization/localizations_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'localizations_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appLocalizationHash() => r'ad025c8d3cc5356849852e217cfee770774fdbfb';
+
+/// provider used to access the AppLocalizations object for the current locale
+///
+/// Copied from [appLocalization].
+@ProviderFor(appLocalization)
+final appLocalizationProvider = Provider<AppLocalizations>.internal(
+  appLocalization,
+  name: r'appLocalizationProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appLocalizationHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppLocalizationRef = ProviderRef<AppLocalizations>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
